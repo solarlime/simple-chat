21f62a472ee520944b9418d24c4ba542d3956d78
solarlime <solarlime@outlook.com>
2020-12-01 07:47:04 +0300

    Changed validation function due to the task

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,22 +1,16 @@
 export default function validation(input) {
   const message = input.nextElementSibling;
-  if (!input.validity.valid || input.validity.rangeUnderflow || !input.value.trim()) {
-    if (input.id === 'title') {
-      if (input.validity.valueMissing) {
-        message.textContent = 'Enter the name, please.';
-      } else {
-        message.textContent = 'This value is invalid.';
-      }
-      message.classList.remove('hidden');
+  if (!input.validity.valid || !input.value.trim()) {
+    if (input.id !== 'title') {
+      message.classList.add('hidden');
+      return true;
     }
-    if (input.id === 'cost') {
-      if (input.validity.rangeUnderflow) {
-        message.textContent = 'The number is invalid. Min value: 0.01.';
-      } else {
-        message.textContent = 'The value is not a number.';
-      }
-      message.classList.remove('hidden');
+    if (input.validity.valueMissing) {
+      message.textContent = 'Enter the name, please.';
+    } else {
+      message.textContent = 'This value is invalid.';
     }
+    message.classList.remove('hidden');
     return false;
   }
   message.classList.add('hidden');
