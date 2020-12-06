7434ea1bfaa9c45d62cba2db050a021df38426e2
solarlime <solarlime@outlook.com>
2020-12-06 22:25:38 +0300

    Added a requests for adding, updating & deleting items

--- src/js/modals.js
+++ src/js/modals.js
@@ -19,6 +19,12 @@ export default class Modals {
     const target = data.find((item) => item.id.toString() === row.getAttribute('data-id'));
     target.done = checkbox.checked;
     Storage.setItems(data);
+
+    // Sending a request
+    const formData = new FormData();
+    Object.entries({ id: row.getAttribute('data-id'), done: checkbox.checked })
+      .forEach((field) => formData.append(field[0], field[1]));
+    Storage.request('update', formData);
     App.update();
   }
 
@@ -26,10 +32,15 @@ export default class Modals {
     const name = document.getElementById('title').value.trim();
     const description = document.getElementById('description').value.trim();
     let data = Storage.getItems();
+    const formData = new FormData();
     if (row) {
       const target = data.find((item) => item.id.toString() === row.getAttribute('data-id'));
       target.name = name;
       target.description = description;
+
+      // Sending a request
+      Object.entries({ id: row.getAttribute('data-id'), name, description }).forEach((field) => formData.append(field[0], field[1]));
+      Storage.request('update', formData);
     } else {
       if (!data) {
         data = [];
@@ -46,9 +57,14 @@ export default class Modals {
         };
         return now.toLocaleString('ru', options);
       });
-      data.push({
+      const newbie = {
         id: id(), done: false, name, description, date: resolveDate(),
-      });
+      };
+      data.push(newbie);
+
+      // Sending a request
+      Object.entries(newbie).forEach((field) => formData.append(field[0], field[1]));
+      Storage.request('new', formData);
     }
     Storage.setItems(data);
     App.update();
@@ -56,6 +72,9 @@ export default class Modals {
   }
 
   static delete(row) {
+    const formData = new FormData();
+    formData.append('id', row.getAttribute('data-id'));
+    Storage.request('delete', formData);
     Storage.setItems(
       // помещаем обратно в localStorage ту его часть, которая не совпадает по id с data-id
       Storage.getItems().filter((item) => item.id.toString() !== row.getAttribute('data-id')),
