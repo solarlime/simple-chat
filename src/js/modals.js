ccead41db5aaa19ba282710bd261c45fb605c30d
solarlime <solarlime@outlook.com>
2021-02-23 01:21:28 +0300

    Removed extra files

--- src/js/modals.js
+++
@@ -1,7 +0,0 @@
-export default class Modals {
-  static login(modal, user, members) {
-    members.push({ name: user });
-    document.querySelector('.greeting').textContent = `You logged as ${user}`;
-    modal.classList.add('hidden');
-  }
-}
